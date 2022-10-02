dcadbc0efbf29f09c98369eb8b04bab004f0ff02
ahmed shafey <96580669+shafey01@users.noreply.github.com>
2022-10-02 23:14:07 +0200

    Update certs_manual.sh

--- certs_manual.sh
+++ certs_manual.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-fullpath=/home/shafey/cert
-
+fullpath=/home/shafey/cert_checker
 grep -Ev "^#|^$" $fullpath/domains.txt | while read  x; do
 
-domain=`echo "$x" | cut -d ' ' -f 1`
-domain_date=`echo "$x" | cut -d ' ' -f 2`
 servername=stage_1
 
 ####################################
 ## Days Equation ##################
 
-untile=`date -d $domain_date "+%s"`
-now=`date "+%s"`
-diff=$(($untile-$now))
-days=$(($diff/(60*60*24)))
+now=$( date +%s )
 
+cert_date=$(sudo openssl x509 -enddate -noout -in /etc/letsencrypt/live/$x/cert.pem | cut -d '=' -f 2 )
+
+
+
+expiry_for_cert=$( date -d "$cert_date" +%s )
+
+days="$(( ($expiry_for_cert - $now) / (3600 * 24) ))"
 
 ############################################
 
-if [[ $days -le 6 ]]
+if [[ $days -le 74 ]]
 then
 
-curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"header","text":{"type":"plain_text","text":"Renew '$domain'","emoji":true}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Days:*\n'$days'"},{"type":"mrkdwn","text":"*Domain:*\n '$domain'"}]},{"type":"section","fields":[{"type":"mrkdwn","text":"*Server:*\n '$servername'"}]}]}' webhook_url
+echo "$days for $x"
+
+#curl -X POST -H 'Content-type: application/json' --data '{"blocks":[{"type":"header","text":{"type":"plain_text","text":"Renew '$domain'","emoji":true}},{"type":"section","fields":[{"type":"mrkdwn","text":"*Days:*\n'$days'"},{"type":"mrkdwn","text":"*Domain:*\n '$domain'"}]},{"type":"section","fields":[{"type":"mrkdwn","text":"*Server:*\n '$servername'"}]}]}' webhook_url
 
 fi
 
 done
+
